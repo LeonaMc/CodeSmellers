7702bedf663f08693a1d0c5e72ebd47a70915358
RichieBrady <richard.brady@ucdconnect.com>
2019-03-12 16:27:54 +0000

    Moved some methods into seperate interfaces

--- Bloatable.java
+++ Bloatable.java
@@ -5,9 +5,7 @@ import java.io.FileNotFoundException;
 import java.text.DecimalFormat;
 
 // find better name for interface, naming after smells pigeon holes its usage.
-public interface Bloatable extends SourceReadable {
+public interface Bloatable extends SourceReadable, Reflectable{
     DecimalFormat df = new DecimalFormat("#.00");
-    public void printReport();
-    public void reflectClass();
     public int countLines(File javaSource) throws FileNotFoundException;
 }
