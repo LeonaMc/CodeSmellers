6a6bd935b9bcca39785abca723fe84be5a82d12e
RichieBrady <richard.brady@ucdconnect.com>
2019-03-14 21:49:01 +0000

    Annotaion for methods who read source files

---
+++ ReadingSource.java
@@ -0,0 +1,9 @@
+package CodeSmellers;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+// Annotation for methods reading source files
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ReadingSource {
+
+}
