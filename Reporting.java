de99fd2bba9365ff5356cad105cbae4dc83bec72
RichieBrady <richard.brady@ucdconnect.com>
2019-03-14 21:50:10 +0000

    Annotaion for methods which return reports of a class

---
+++ Reporting.java
@@ -0,0 +1,9 @@
+package CodeSmellers;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+// for reporting methods
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Reporting {
+}
