f80d21962e7b24b498844a52e066f37dd1a8ff13
RichieBrady <richard.brady@ucdconnect.com>
2019-03-14 21:44:51 +0000

    Annotaion for class loader method

---
+++ ClassLoader.java
@@ -0,0 +1,8 @@
+package CodeSmellers;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+// Annotation for class loader method
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ClassLoader {
+}
