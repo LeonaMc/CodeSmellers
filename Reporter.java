10f314a68b53540c19c7a7882809e7305f42c192
RichieBrady <richard.brady@ucdconnect.com>
2019-03-15 09:48:05 +0000

    Added method to return Report object

--- Reporter.java
+++ Reporter.java
@@ -2,7 +2,13 @@ package CodeSmellers;
 
 public class Reporter implements Reportable {
     @Override
-    public void returnReport() {
+    public Report returnReport() {
+
+        return new Report();
+    }
+
+    @Override
+    public void formatData() {
 
     }
 }
