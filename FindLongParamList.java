5cbc82d5ae384c2ed0c827a72b9e73485c0785e4
RichieBrady <richard.brady@ucdconnect.com>
2019-03-18 21:42:47 +0000

    Added implmentation of code smell, needs work on data being found

--- FindLongParamList.java
+++ FindLongParamList.java
@@ -1,24 +1,62 @@
 package CodeSmellers;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class FindLongParamList implements Countable{
-    private ArrayList<Class> classArrayList;
+public class FindLongParamList implements Reflectable{
+    private ArrayList<Class> loadedClasses;
+    private ArrayList<Class> cleanClasses;
+    private HashMap<Class, ArrayList<Method>> longParamMap;
 
-    public FindLongParamList(ArrayList<Class> classArrayList){
-        this.classArrayList = new ArrayList<>();
-        this.classArrayList.addAll(classArrayList);
+    public FindLongParamList(ArrayList<Class> loadedClasses){
+        this.loadedClasses = new ArrayList<>();
+        this.loadedClasses.addAll(loadedClasses);
+        cleanClasses = new ArrayList<>();
+        longParamMap = new HashMap<>();
     }
 
-    public int countMethodParams(Method method) throws FileNotFoundException {
-        return method.getParameterCount();
+    private void findLongParamMethods(Class cls) throws FileNotFoundException {
+        ArrayList<Method> longParamMethods = new ArrayList<>();
+        for (Method method: cls.getDeclaredMethods()){
+            if(method.getParameterCount() >= 4){
+                longParamMethods.add(method);
+            }
+        }
+        if(longParamMethods.size() > 0){
+            longParamMap.put(cls,longParamMethods);
+        }
+        else{
+            cleanClasses.add(cls);
+        }
     }
 
     @Override
-    public int countComponents(Class cls) throws FileNotFoundException {
-        return 0;
+    public void reflectClass() {
+        for(Class cls: loadedClasses){
+            try {
+                findLongParamMethods(cls);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        loadedClasses.removeAll(cleanClasses);
+    }
+
+    // test
+    public void printTestReport(){
+        for(Class cls : loadedClasses){
+            System.out.println("Long parameter methods in class " + cls.getSimpleName());
+            for(Method method : longParamMap.get(cls)){
+                System.out.print("\nParameter count for " + method.getName() +" is " + method.getParameterCount() + "\n"
+                        + "Parameters are\n");
+                for (Parameter parameter : method.getParameters()){
+                    System.out.println(parameter.getName() + " of type " + parameter.getType());
+                }
+            }
+            System.out.println("Suggested to create a new methods and split functionality in class " + cls.getSimpleName() + "\n"); // template message
+        }
     }
 }
