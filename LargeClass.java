b43f36b08db9d12397951599b8d67027c2cc2500
RichieBrady <richard.brady@ucdconnect.com>
2019-03-22 10:22:39 +0000

    Added Report object to collect effected classes and smell data

--- LargeClass.java
+++ LargeClass.java
@@ -2,22 +2,23 @@ package CodeSmellers;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 // Refactor Class to take class and java arrays in constructor, give length of file in report
-public class FindLargeClass implements Bloatable {
+public class LargeClass implements Inspectable {
     private ArrayList<Class> loadedClasses;
     private ArrayList<File> javaSource; // may not need this!
     private int numberOfSourceFiles;
-    private HashMap<Class, ArrayList<String>> classReports; // see reflectClass()
+    private LineCounter lineCounter;
+    private Report report;
 
-    FindLargeClass(ArrayList<File> javaSource, ArrayList<Class> loadedClasses){
+    LargeClass(ArrayList<File> javaSource, ArrayList<Class> loadedClasses){
         this.javaSource = new ArrayList<>();
         this.loadedClasses = new ArrayList<>();
         this.javaSource.addAll(javaSource);
         this.loadedClasses.addAll(loadedClasses);
+        lineCounter = new LineCounter();
+        report = new Report();
         numberOfSourceFiles = javaSource.size();
-        classReports = new HashMap<>();
     }
 
     @Override // no implementation for this class?
@@ -25,33 +26,11 @@ public class FindLargeClass implements Bloatable {
         return null;
     }
 
-    @ReadingSource
-    @Override
-    public int countLines(File javaSource) throws FileNotFoundException {
-        FileReader fileReader = new FileReader(javaSource);
-        LineNumberReader numberReader = new LineNumberReader(fileReader);
-        int lines = 0;
-        try {
-            while (numberReader.readLine() != null) {
-                lines++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            numberReader.close();
-            fileReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return lines;
-    }
-
     public void findLargeFiles(){
         for (int i = 0; i < javaSource.size(); i++){
             try {
                 int upperBound = 200;
-                if(!(countLines(javaSource.get(i)) > upperBound)){ // files not large
+                if(!(lineCounter.countLines(javaSource.get(i)) > upperBound)){ // files not large
                     loadedClasses.remove(i);
                     javaSource.remove(i);
                 }
@@ -59,6 +38,7 @@ public class FindLargeClass implements Bloatable {
                 e.printStackTrace();
             }
         }
+        report.setEffectedClasses(loadedClasses);
     }
 
     @Reflecting
@@ -66,15 +46,20 @@ public class FindLargeClass implements Bloatable {
     public void reflectClass() {
         // if long class, reflect and find ways to improve e.g. count number of primitives and methods advise on results
         for (Class cls : loadedClasses){
-            ArrayList<String> report = new ArrayList<>();
             if(cls.getDeclaredMethods().length > 10){ // placeholder value, not sure how many methods is too many
-                report.add(cls.getName() + " has " + cls.getDeclaredMethods().length + " Methods"); // need better report messages
+                String reportMessage = cls.getSimpleName() + " has " + cls.getDeclaredMethods().length + " Methods";
+                report.getCodeSmellData().put(cls,reportMessage); // need better report messages
             }
             if (cls.getDeclaredFields().length > 10){
-                report.add(cls.getName() + " has " + cls.getDeclaredFields().length + " Declared Fields");
+                String reportMessage = cls.getSimpleName() + " has " + cls.getDeclaredFields().length + " Declared Fields";
+                report.setCodeSmellData(cls,reportMessage);
             }
-            classReports.put(cls,report);
+            // if < 10 fields or methods indicates long method smell
         }
+    }
+
+    public Report getReport() {
+        return report;
     }
 
     public void printTestReport(){
@@ -82,13 +67,12 @@ public class FindLargeClass implements Bloatable {
         for (Class cls: loadedClasses){
             String newline = "\n";
             System.out.println("Java Class: " + cls.getSimpleName() + " is too large " + newline);
-            for(String report: classReports.get(cls)){
-                System.out.println(report + newline);
-            }
+           //for(String report: classReports.get(cls)){
+           //    System.out.println(report + newline);
+           //}
             bloated++;
         }
-        double percent = (bloated /numberOfSourceFiles) * 100;
-        System.out.println(Bloatable.df.format(percent) + "% of the files in your project are bloated");
+        double percent = (bloated/numberOfSourceFiles) * 100;
+        System.out.println(Inspectable.df.format(percent) + "% of the files in your project are bloated");
     }
-
 }
