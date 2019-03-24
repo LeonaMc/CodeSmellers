8e203b7f7fb6f84a3062b8addeff3413be3384c4
RichieBrady <richard.brady@ucdconnect.com>
2019-03-24 10:20:36 +0000

    Fixed regex problem, need to finalise what data is added to report

--- LongMethods.java
+++ LongMethods.java
@@ -13,11 +13,14 @@ public class LongMethods implements Inspectable {
     private HashMap<Class, Method[]> classMethods;
     private ArrayList<Class> loadedClasses;
     private ArrayList<File> javaSourceFiles;
-    private Stack<Integer> bracketStack = new Stack<>();
+    private Stack<Integer> bracketStack;
+    private Report report;
 
     LongMethods(ArrayList<Class> loadedClasses, ArrayList<File> javaSource) {
         this.loadedClasses = new ArrayList<>(loadedClasses);
         this.javaSourceFiles = new ArrayList<>(javaSource);
+        report = new Report();
+        bracketStack = new Stack<>();
         classMethods = new HashMap<>();
     }
 
@@ -26,6 +29,11 @@ public class LongMethods implements Inspectable {
             classMethods.put(cls, cls.getDeclaredMethods());
         }
     }
+
+    public Report getReport() {
+        return report;
+    }
+
     // Doesn't find body if method body is on one line or if no parameters
     public int getMethodBody(int startLine, File javaSource) throws IOException {
         FileInputStream fileInputStream = new FileInputStream(javaSource);
@@ -37,7 +45,7 @@ public class LongMethods implements Inspectable {
         while ((line = input.readLine()) != null) {
             i++;
             if (i == startLine) {
-                System.out.println(line);
+                //System.out.println(line);
                 foundStart = true;
                 for (int j = 0; j < line.length(); j++) {
                     if (line.charAt(j) == '{') {
@@ -45,14 +53,14 @@ public class LongMethods implements Inspectable {
                     }
                 }
             } else if (foundStart) {
-                System.out.println(line);
+                //System.out.println(line);
                 for (int j = 0; j < line.length(); j++) {
                     if (line.charAt(j) == '{') {
                         bracketStack.push((int) line.charAt(j));
                     } else if (line.charAt(j) == '}') {
                         bracketStack.pop();
                         if (bracketStack.isEmpty()) {
-                            endLine = i;
+                            endLine = i+1;
                             foundEnd = true;
                         }
                     }
@@ -76,31 +84,29 @@ public class LongMethods implements Inspectable {
                 myMethod = method;
             }
         }
-        System.out.println("myMethod " + myMethod.getName());
-        String buildRegex = Modifier.toString(myMethod.getModifiers());
-        System.out.println("BuildRegex " + buildRegex);
+        String buildRegex;
+        buildRegex = Modifier.toString(myMethod.getModifiers()); // add catch for null getModifiers
+        buildRegex = buildRegex + " " + myMethod.getReturnType().getSimpleName();
+        //System.out.println("BuildRegex " + buildRegex);
         String regex = buildRegex + " " + keyword;
-        System.out.println("Regex " + regex);
-        Pattern methodPattern = Pattern.compile(regex); // regular expression to find first line of method
+
+        Pattern methodPattern = Pattern.compile(Pattern.quote(regex)); // regular expression to find first line of method
         Matcher matcher = methodPattern.matcher("");
         String line;
         int startLine = 0;
         int i = 0;
-
         try {
             while ((line = input.readLine()) != null) {
 
                 matcher.reset(line);
                 i++;
-                System.out.println(line);
                 while (matcher.find()) {
-                    System.out.println(line);
                     startLine = i;
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
 
         try {
             fileInputStream.close();
@@ -115,8 +121,11 @@ public class LongMethods implements Inspectable {
             e.printStackTrace();
         }
 
-        System.out.println("Method " + keyword + " in " + javaSource.getName() + " is " + (endLine - startLine) + " lines long\n");
-        System.out.println("Method starts at line " + startLine + " and ends at line " + endLine + " in " + javaSource.getName());
+        if(endLine-startLine > 30){
+            return "Method " + keyword + " in " + javaSource.getName() + " is " + (endLine - startLine) + " lines long\n" +
+                   "Method starts at line " + startLine + " and ends at line " + endLine + " in " + javaSource.getName();
+        }
+        else
         return null;
     }
 
@@ -124,26 +133,35 @@ public class LongMethods implements Inspectable {
     @Override
     public void reflectClass() {
         getClassMethods();
-        int fileToTest = 1; // change value to check different class
-        //new File("src\\methodsToText").mkdir();
-        System.out.println(loadedClasses.get(fileToTest).getSimpleName());
-        String keyword = loadedClasses.get(fileToTest).getDeclaredMethods()[0].getName();
-        System.out.println("Keyword " + keyword);
+        ArrayList<Class> cleanClasses = new ArrayList<>();
 
-        try {
-            getKeyword(keyword, javaSourceFiles.get(fileToTest));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        /*for(Class cls : loadedClasses){
+        for(Class cls : loadedClasses){
+            HashMap<Method,String> longMethodData = new HashMap<>();
             Method[] tempClassMethods = classMethods.get(cls);
-            for(Method meth: tempClassMethods){
+            String effectedMethodMessage = null;
+            for(Method method: tempClassMethods){
                 try {
-                    getKeyword(meth.getName(), javaSourceFiles.get(index));
+                    effectedMethodMessage = getKeyword(method.getName(), javaSourceFiles.get(loadedClasses.indexOf(cls)));
+                    if(effectedMethodMessage != null){
+                        longMethodData.put(method, effectedMethodMessage);
+                    }
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
             }
-        }*/
+            if (!longMethodData.isEmpty()){
+                report.putCodeSmellData(cls, longMethodData);
+            }
+        }
+
+        for(Class cls : loadedClasses){
+            if (!report.getCodeSmellData().containsKey(cls)){
+                cleanClasses.add(cls);
+            }
+        }
+        if(!cleanClasses.isEmpty()){
+            loadedClasses.removeAll(cleanClasses);
+            report.setEffectedClasses(loadedClasses);
+        }
     }
 }
