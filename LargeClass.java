9835dabadb19ba94c9a169a6b1078a86e724098f
RichieBrady <richard.brady@ucdconnect.com>
2019-03-24 10:18:48 +0000

    Cleaned up unused code

--- LargeClass.java
+++ LargeClass.java
@@ -3,11 +3,9 @@ package CodeSmellers;
 import java.io.*;
 import java.util.ArrayList;
 
-// Refactor Class to take class and java arrays in constructor, give length of file in report
 public class LargeClass implements Inspectable {
     private ArrayList<Class> loadedClasses;
-    private ArrayList<File> javaSource; // may not need this!
-    private int numberOfSourceFiles;
+    private ArrayList<File> javaSource;
     private LineCounter lineCounter;
     private Report report;
 
@@ -18,7 +16,6 @@ public class LargeClass implements Inspectable {
         this.loadedClasses.addAll(loadedClasses);
         lineCounter = new LineCounter();
         report = new Report();
-        numberOfSourceFiles = javaSource.size();
     }
 
     @Override // no implementation for this class?
@@ -44,7 +41,7 @@ public class LargeClass implements Inspectable {
     @Reflecting
     @Override
     public void reflectClass() {
-        // if long class, reflect and find ways to improve e.g. count number of primitives and methods advise on results
+        // if long class, reflect and find ways to improve e.g. count number of primitives and methods advise on results. If class large and has long methods
         for (Class cls : loadedClasses){
             if(cls.getDeclaredMethods().length > 10){ // placeholder value, not sure how many methods is too many
                 String reportMessage = cls.getSimpleName() + " has " + cls.getDeclaredMethods().length + " Methods";
@@ -52,27 +49,12 @@ public class LargeClass implements Inspectable {
             }
             if (cls.getDeclaredFields().length > 10){
                 String reportMessage = cls.getSimpleName() + " has " + cls.getDeclaredFields().length + " Declared Fields";
-                report.setCodeSmellData(cls,reportMessage);
+                report.putCodeSmellData(cls,reportMessage);
             }
-            // if < 10 fields or methods indicates long method smell
         }
     }
 
     public Report getReport() {
         return report;
-    }
-
-    public void printTestReport(){
-        double bloated = 0;
-        for (Class cls: loadedClasses){
-            String newline = "\n";
-            System.out.println("Java Class: " + cls.getSimpleName() + " is too large " + newline);
-           //for(String report: classReports.get(cls)){
-           //    System.out.println(report + newline);
-           //}
-            bloated++;
-        }
-        double percent = (bloated/numberOfSourceFiles) * 100;
-        System.out.println(Inspectable.df.format(percent) + "% of the files in your project are bloated");
     }
 }
