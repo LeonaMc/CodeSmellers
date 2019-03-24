6b94040d9a0a614fb529a0e59cca1bd98185cb68
RichieBrady <richard.brady@ucdconnect.com>
2019-03-24 10:25:58 +0000

    Added throws exceMethod now throws file not found exception

--- SourceReadable.java
+++ SourceReadable.java
@@ -2,6 +2,7 @@ package CodeSmellers;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public interface SourceReadable {
     public String getKeyword(String keyword, File javaSource) throws FileNotFoundException;
