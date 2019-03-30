8a175a552b79f1839619342fd573ed47799d7552
RichieBrady <richard.brady@ucdconnect.com>
2019-03-30 20:35:00 +0000

    Nothing changed

--- DuplicateCode.java
+++ DuplicateCode.java
@@ -1,7 +1,12 @@
 package CodeSmellers;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class DuplicateCode {
-    public DuplicateCode(){
+    public DuplicateCode(ArrayList<Class> loadedClasses, ArrayList<File> javaSource){
 
     }
+
+
 }
