2843c9ee3051687809b1c95474f89a4992470546
RichieBrady <richard.brady@ucdconnect.com>
2019-03-30 20:30:40 +0000

    Added comment

--- Countable.java
+++ Countable.java
@@ -2,7 +2,7 @@ package CodeSmellers;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-
+// implemented by LineCounter
 public interface Countable {
     public int countLines(File javaSource) throws FileNotFoundException;
 }
