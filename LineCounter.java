d428fba1418efd3832cca9483ec8f185ff5ebebb
RichieBrady <richard.brady@ucdconnect.com>
2019-03-30 20:36:28 +0000

    Added method to count multiline comments length and comments

--- LineCounter.java
+++ LineCounter.java
@@ -1,11 +1,13 @@
 package CodeSmellers;
 
 import java.io.*;
+import java.util.Stack;
 
 public class LineCounter implements Countable {
+    private Stack<String> characterStack;
 
     public LineCounter(){
-
+        characterStack = new Stack<>();
     }
 
     @Override
@@ -27,5 +29,48 @@ public class LineCounter implements Countable {
             e.printStackTrace();
         }
         return lines;
+    }
+
+    public int countBody(File javaSource) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(javaSource); // new input stream
+        BufferedReader input = new BufferedReader(new InputStreamReader(fileInputStream)); // Buffered reader for large files
+        String line; // current line from reader
+        int lineCounter = 0; // current line number
+        int startCommentLine = 0; // line number of /*
+        int endCommentLine = 0; // line number of */
+        boolean startComment = false; // when true /* found
+        boolean foundEnd = false; // when true */ found
+        while ((line = input.readLine()) != null) { // while reader has lines to read
+            lineCounter++; // increment on each new line
+            for (int j = 0; j < line.length(); j++) { // iterate through characters of line to find start of /*
+                if (line.charAt(j) == '/') { // if current character is /
+                    if((j+1) < line.length()){ // check if j+1 is out of bounds
+                      if (line.charAt(j+1) == '*'){ // if j+1 is not out of bounds check if next char is *
+                          String openingComment = "/*"; // set string to opening multiline comment
+                          characterStack.push(openingComment); // push to stack
+                          if(!startComment) { //
+                              startCommentLine = lineCounter; // set starting lineNumber of comment
+                              startComment = true;
+                          }
+                      }
+                    }
+                } else if (line.charAt(j) == '*') { // check if next char is start of closing comment
+                    if((j+1) < line.length()){ // make sure j+1 is not out of bounds
+                        if(line.charAt(j+1) == '/'){
+                            characterStack.pop(); // if */ found pop last added /*
+                        }
+                    }
+                    if (characterStack.isEmpty()) { // if stack is empty multiline comment end found
+                        endCommentLine = lineCounter; // set end comment line number
+                        foundEnd = true;
+                        startComment = false;
+                    }
+                }
+            }
+            if (foundEnd) {
+                break;
+            }
+        }
+        return endCommentLine; //endCommentLine - startCommentLine; //return this for comment length
     }
 }
