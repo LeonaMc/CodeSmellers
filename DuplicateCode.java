5c217740fbbff7b84d956f45c03b0b1863254da5
RichieBrady <richard.brady@ucdconnect.com>
2019-03-31 11:25:05 +0100

    Reminder to start asap

--- DuplicateCode.java
+++ DuplicateCode.java
@@ -2,7 +2,7 @@ package CodeSmellers;
 
 import java.io.File;
 import java.util.ArrayList;
-
+//
 public class DuplicateCode {
     public DuplicateCode(ArrayList<Class> loadedClasses, ArrayList<File> javaSource){
 
