a16f7e4a17f4bcd5b1b51c60e442312ec5e5094e
RichieBrady <richard.brady@ucdconnect.com>
2019-04-01 00:27:03 +0100

    Implmentation finished. Class now collects proper data

--- LongParamList.java
+++ LongParamList.java
@@ -2,17 +2,22 @@ package CodeSmellers;
 
 import java.io.FileNotFoundException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.ArrayList;
+import java.util.HashMap;
+
 // needs further implementation
 public class LongParamList implements Reflectable{
     private ArrayList<Class> loadedClasses;
     private ArrayList<Class> cleanClasses;
+    private HashMap<Class, ArrayList<Method>> methodHash;
     private Report report;
 
     public LongParamList(ArrayList<Class> loadedClasses){
         this.loadedClasses = new ArrayList<>();
         this.loadedClasses.addAll(loadedClasses);
         cleanClasses = new ArrayList<>();
+        methodHash = new HashMap<>();
         report = new Report();
     }
 
@@ -24,7 +29,7 @@ public class LongParamList implements Reflectable{
             }
         }
         if(longParamMethods.size() > 0){ // if current class has effected methods
-            report.putCodeSmellData(cls, longParamMethods); // add data to report
+            methodHash.put(cls, longParamMethods); // add data to report
         }
         else{
             cleanClasses.add(cls); // else add the clean class
@@ -42,6 +47,23 @@ public class LongParamList implements Reflectable{
         }
         loadedClasses.removeAll(cleanClasses); // remove clean classes so only effected classes left
         report.setEffectedClasses(loadedClasses); // add effected classes classes to report
+
+        for (Class cls : loadedClasses) {
+            ArrayList<Method> effectedMethods = new ArrayList<>();
+            effectedMethods.addAll(methodHash.get(cls));
+            String effectedMethodMessage = "\nName of effected class = " + cls.getSimpleName() +
+                                            "\nNumber of effected methods = " + effectedMethods.size();
+            double methodPercent = ((double) effectedMethods.size() / (double) cls.getDeclaredMethods().length) * 100;
+            effectedMethodMessage += "\n" + report.df.format(methodPercent) + "% of methods effected\nMethod names\n";
+            StringBuilder builder = new StringBuilder(effectedMethodMessage);
+            for (Method method : effectedMethods) {
+                builder.append("\n" + method.getName() + " has " + method.getParameters().length + " parameters\nParameter types are\n");
+                for (Class parameter : method.getParameterTypes()){
+                    builder.append(parameter.getTypeName() + "\n");
+                }
+            }
+            report.putCodeSmellData(cls, builder.toString());
+        }
     }
     // returns report
     public Report getReport() {
