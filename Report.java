833968563c594caa65f90d612a4cf25e8c6065ae
RichieBrady <richard.brady@ucdconnect.ie>
2019-04-05 22:42:24 +0100

    Added comments

--- Report.java
+++ Report.java
@@ -5,7 +5,7 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 public class Report<T> {
-    DecimalFormat df = new DecimalFormat("#.00");
+    DecimalFormat df = new DecimalFormat("#0.00");
     private ArrayList<Class> affectedClasses; // holds only effected classes of code smell report belongs to
     private HashMap<Class,T> reportData; // holds data found from code smell inspection. All keys are held in affected classes
     private HashMap<Method,String> longMethodData;
@@ -17,34 +17,39 @@ public class Report<T> {
         longMethodData = new HashMap<>();
         percentOfFilesAffected = 0.0;
     }
-    // adds only classes affected by smell
+    /* method is called by report object inside each smell class
+    *  only classes affected by a smell are added*/
     public void setAffectedClasses(ArrayList<Class> affectedClasses){
         this.affectedClasses.addAll(affectedClasses);
     }
-    //
+    /*method is called by report object inside each smell class
+    * Affected class is key
+    * formatted data is added after smell class has checked all files
+    * data is any information collected relevant to smell e.g length of method and start/end line for long method smell*/
     public void putReportData(Class key, T data){
         this.reportData.put(key, data);
     }
-
+    // used by long method smell only, affected method is key data is method name etc
     public void putLongMethodData(Method key, String data){
         this.longMethodData.put(key, data);
     }
-
+    // return list of affected classes for an individual code smell
     public ArrayList<Class> getAffectedClasses() {
         return affectedClasses;
     }
-
+    // returns map which holds data collected in code smell class, see putReportData
     public HashMap<Class,T> getReportData() {
         return reportData;
     }
-
+    // returns map for method data found in long method class
     public HashMap<Method, String> getLongMethodData(){
         return longMethodData;
     }
+    // sets percentage of files affected compared to overall class files loadedClassSize = loadedClasses.size()
     public void setPercentage(int loadedClassSize){
         percentOfFilesAffected = ((double) affectedClasses.size()/(double)loadedClassSize)*100;
     }
-
+    // return percentage of files affected by a smell
     public Double getPercentage(){
         return percentOfFilesAffected;
     }
