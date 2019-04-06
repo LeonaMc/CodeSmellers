f44e63193e8ca868628d8f61580c4744e3f3e994
RichieBrady <richard.brady@ucdconnect.ie>
2019-04-06 13:25:31 +0100

    Fully implemented, adds proper data to report

--- TooManyLiterals.java
+++ TooManyLiterals.java
@@ -18,13 +18,20 @@ public class TooManyLiterals implements Reflectable {
 
     @Override
     public void reflectClass() {
+        ArrayList<Class> cleanClasses = new ArrayList<>();
+        int namedConstantCount = 0;
+        int primitiveLiterals = 0;
         for (Class cls : loadedClasses) { // for each class
-            System.out.println("Class: " + cls.getSimpleName()); // test print name
-            for (final Field field : cls.getDeclaredFields()) { // for each field in class set filed to final
+            for (final Field field : cls.getDeclaredFields()) { // for each field in class set file to final
+                if(Modifier.isStatic(field.getModifiers()) && Modifier.isStatic(field.getModifiers())){
+                    namedConstantCount++;
+                }
                 if(field.getType().isPrimitive() && !field.isSynthetic()){ // only get primitives and reject synthetic variables(compiler made)
                     field.setAccessible(true); // if field modifier is private set to accessible
-                    try{ // report needs to be implemented
-                        System.out.println(Modifier.toString(field.getModifiers())+" "+field.getType()+" "+field.getName()+" = "+field.get(cls).toString()); // test print data
+                    try{
+                        if(field.get(cls) != null){ // check if primitive field has value assigned
+                            primitiveLiterals++;
+                        }
                     }
                     catch (IllegalArgumentException e){
                         continue;
@@ -33,12 +40,42 @@ public class TooManyLiterals implements Reflectable {
                     }
                 }
             }
-            System.out.print("\n");
+
+            if(namedConstantCount > 0 || primitiveLiterals < 5){ // heuristic may need to be changed
+                cleanClasses.add(cls);
+            }
+            namedConstantCount = 0;
+            primitiveLiterals = 0;
+        }
+
+        loadedClasses.removeAll(cleanClasses);
+        report.setAffectedClasses(loadedClasses);
+        String message;
+        for (Class cls : loadedClasses) { // for each class
+            message = "Class: " + cls.getSimpleName() + "\n"; // test print name
+            for (final Field field : cls.getDeclaredFields()) { // for each field in class set file to final
+                if(field.getType().isPrimitive() && !field.isSynthetic()){ // only get primitives and reject synthetic variables(compiler made)
+                    field.setAccessible(true); // if field modifier is private set to accessible
+                    try{ // report needs to be implemented
+                        message += Modifier.toString(field.getModifiers())+" "+field.getType()+" "+field.getName()+" = "+field.get(cls).toString()+"\n"; // test print data
+                    }
+                    catch (IllegalArgumentException e){
+                        continue;
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            report.putReportData(cls,message);
         }
     }
+
     // shortens code for every call to getSimpleName
     private String getFieldSimpleName(Field field){
         return field.getType().getSimpleName();
     }
 
+    public Report getReport(){
+        return report;
+    }
 }
