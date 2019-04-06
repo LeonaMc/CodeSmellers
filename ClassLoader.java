e098bf70ba3e9e45e88f03d63d58aa1cdb375b21
LeonaMc <leona.mc-nulty@ucdconnect.ie>
2019-04-06 17:44:09 +0100

    Delete ClassLoader.java

--- ClassLoader.java
+++
@@ -1,9 +0,0 @@
-package CodeSmellers;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-// Annotation for class loader method
-@Retention(RetentionPolicy.RUNTIME)
-public @interface ClassLoader {
-    //empty
-}
