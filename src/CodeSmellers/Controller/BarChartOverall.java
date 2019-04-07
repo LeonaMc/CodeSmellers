5ff015de3f387ed7871cf37afeb807a5be9bba95
LeonaMc <leona.mc-nulty@ucdconnect.ie>
2019-04-07 03:19:12 +0100

    Visualises the smell percentages using BarChartCalc class

--- src/CodeSmellers/Controller/BarChartOverall.java
+++ src/CodeSmellers/Controller/BarChartOverall.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -27,7 +29,7 @@ import CodeSmellers.Model.BarChartCalc;
 public class BarChartOverall implements Initializable{
 	
     @FXML
-    private BarChart<?, ?> SmellChart;
+    private BarChart<?, ?> smellChart;
 
     @FXML
     private CategoryAxis x;
@@ -35,52 +37,78 @@ public class BarChartOverall implements Initializable{
     @FXML
     private NumberAxis y;
     
-    BarChartCalc bc = new BarChartCalc();
+    BarChartCalc barChart = new BarChartCalc();
 
     @Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+    	
+    	 XYChart.Series codeSmellBar = new XYChart.Series<>();
 
-
-        XYChart.Series smell1 = new XYChart.Series<>();
-        XYChart.Series smell2 = new XYChart.Series<>();
-		XYChart.Series smell3 = new XYChart.Series<>();
-		XYChart.Series smell4 = new XYChart.Series<>();
-		XYChart.Series smell5 = new XYChart.Series<>();
-		XYChart.Series smell6 = new XYChart.Series<>();
-		XYChart.Series smell7 = new XYChart.Series<>();
-		XYChart.Series smell8 = new XYChart.Series<>();
-		XYChart.Series smell9 = new XYChart.Series<>();
-		XYChart.Series smell10 = new XYChart.Series<>();
-		XYChart.Series smell11 = new XYChart.Series<>();
-		
-		smell1.setName("Duplicate Code");
-		smell2.setName("Bloated Code");
-		smell3.setName("Feature Envy");
-		smell4.setName("Inappropriate Intimacy");
-		smell5.setName("Too Many Literals");
-		smell6.setName("Primitive Obsessions");
-		smell7.setName("God Complex");
-		smell8.setName("Lazy Class/Freeloader");
-		smell9.setName("Comments");
-		smell10.setName("Switch Statements");
-		smell11.setName("Data Class");
-
-        
-		smell1.getData().add(new XYChart.Data("Duplicate Code", bc.result()));
-		smell2.getData().add(new XYChart.Data("Bloated Code", 20.00));
-		smell3.getData().add(new XYChart.Data("Feature Envy", 30.00));
-		smell4.getData().add(new XYChart.Data("Inappropriate Intimacy", 10.00));
-		smell5.getData().add(new XYChart.Data("Too Many Literals", 20.00));
-		smell6.getData().add(new XYChart.Data("Primitive Obsessions", 50.00));
-		smell7.getData().add(new XYChart.Data("God Complex", 60.00));
-		smell8.getData().add(new XYChart.Data("Lazy Class/Freeloader", 30.00));
-		smell9.getData().add(new XYChart.Data("Comments", 10.00));
-		smell10.getData().add(new XYChart.Data("Switch Statements", 2.00));
-		smell11.getData().add(new XYChart.Data("Data", 1.00));
-		
-		SmellChart.getData().addAll(smell1, smell2, smell3, smell4, smell5, smell6, smell7, smell8, smell9, smell10, smell11);
-		
+ 		// 12 code smells
+ 	    List<String> codeSmells = Arrays.asList("Feature Envy", "Large Class", "Long Method", "Long Parametre",
+ 	    										"Duplicate Code", "Inappropriate Intimacy", "Too Many Literals", 
+ 	    										"Primitive Obsessions", "God Complex", "Lazy Class/Freeloader", 
+ 	    										"Comments", "Switch Statements", "Data Clump");
+ 	    
+ 	    // Set name and percentage to bar chart
+ 		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(0), barChart.getResults().get(0)));
+ 		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(1), barChart.getResults().get(1)));
+ 		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(2), barChart.getResults().get(2)));
+ 		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(3), barChart.getResults().get(1)));
+ 		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(4), barChart.getResults().get(1)));
+ 		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(5), barChart.getResults().get(2)));
+ 		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(6), barChart.getResults().get(2)));
+ 		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(7), barChart.getResults().get(1)));
+ 		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(8), barChart.getResults().get(2)));
+		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(9), barChart.getResults().get(2)));
+ 		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(10), barChart.getResults().get(2)));
+ 		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(11), barChart.getResults().get(1)));
+ 		codeSmellBar.getData().add(new XYChart.Data(codeSmells.get(12), barChart.getResults().get(1)));
+ 		
+ 		// Add data to bar chart
+ 		smellChart.getData().addAll(codeSmellBar);
+ 		
+ 		// CSS
+ 		Node n = smellChart.lookup(".data0.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: red");
+ 	    
+ 	    n = smellChart.lookup(".data1.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: orange");
+ 	    
+ 	    n = smellChart.lookup(".data2.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: yellow");
+ 	    
+ 	    n = smellChart.lookup(".data3.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: green");
+ 	    
+ 	    n = smellChart.lookup(".data4.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: pink");
+ 	    
+ 	    n = smellChart.lookup(".data5.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: purple");
+ 	    
+ 	    n = smellChart.lookup(".data6.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: blue");
+ 	    
+ 	    n = smellChart.lookup(".data7.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: orange");
+ 	    
+ 	    n = smellChart.lookup(".data8.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: yellow");
+ 	    
+ 	    n = smellChart.lookup(".data9.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: green");
+ 	    
+ 	    n = smellChart.lookup(".data10.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: pink");
+ 	    
+ 	    n = smellChart.lookup(".data11.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: purple");
+ 	    
+ 	    n = smellChart.lookup(".data12.chart-bar");
+ 	    n.setStyle("-fx-bar-fill: blue");
+ 	   
 }
 		
 		
