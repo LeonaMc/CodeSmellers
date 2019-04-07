a3eefebb1293a07d5f0988216e1cae20a4c53e38
RichieBrady <richard.brady@ucdconnect.ie>
2019-04-07 23:50:11 +0100

    Test

--- src/CodeSmells/Main.java
+++ src/CodeSmells/Main.java
@@ -29,7 +29,7 @@ public class Main extends Application { //
         String newline = "\n";
         String[] packageArray = new String[2];
         DirectoryReader directoryReader = new DirectoryReader();
-        String directoryPath = ""; // Add path to root of directory here
+        String directoryPath = "C:\\Users\\RickTheRuler\\git\\CodeSmellers\\"; // Add path to root of directory here
         directoryReader.getFiles(directoryPath);
 
         // Splash Screen
