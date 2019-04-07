c340b8d86c8206d8ec5c19036ff8efd73838ccd9
LeonaMc <leona.mc-nulty@ucdconnect.ie>
2019-04-07 03:14:53 +0100

    Bar chart uses data from file

--- src/CodeSmellers/Model/BarChartCalc.java
+++ src/CodeSmellers/Model/BarChartCalc.java
@@ -3,64 +3,81 @@ package CodeSmellers.Model;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import CodeSmellers.DirectoryReader;
 import CodeSmellers.Inspection;
-import CodeSmellers.*;
 
 public class BarChartCalc{
-	
-    String newline = "\n";
-    String[] packageArray = new String[2];
-    DirectoryReader directoryReader = new DirectoryReader();
-    String directoryPath = "C:\\Eclipse\\ActuallyJavaFX"; // Add path to root of directory here
-    
-    public double getResults() {
-    
-    directoryReader.getFiles(directoryPath);
+  
+	ArrayList<Double> percentages = new ArrayList<Double>();
+     
+    public  ArrayList<Double> getResults() {
+    	
+    	String newline = "\n";
+        String[] packageArray = new String[2];
+        DirectoryReader directoryReader = new DirectoryReader();
+        String directoryPath = "C:\\Eclipse\\ActuallyJavaFX"; // Add path to root of directory here
+        directoryReader.getFiles(directoryPath);
+                
+        if(directoryReader.getDirectoryLevel() > 0){
+            packageArray = directoryReader.getClasspath(directoryReader.getClassArrayList().get(0).getPath());
+        }
+        else{
+            packageArray[0] = null;
+            packageArray[1] = directoryPath;
+        }
+
+        try {
+            directoryReader.loadClasses(packageArray);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         
-    if(directoryReader.getDirectoryLevel() > 0){
-        packageArray = directoryReader.getClasspath(directoryReader.getClassArrayList().get(0).getPath());
-    }
-    else{
-        packageArray[0] = null;
-        packageArray[1] = directoryPath;
-    }
+        ArrayList<Class> loadedClasses = new ArrayList<>(directoryReader.getLoadedClasses()); // classes ready for reflection
+        ArrayList<File> javaSource = new ArrayList<>(directoryReader.getJavaSourceArrayList()); // can read java files as text
 
-    try {
-        directoryReader.loadClasses(packageArray);
-    } catch (FileNotFoundException e) {
-        e.printStackTrace();
-    }
-	
+        Inspection inspection = new Inspection(loadedClasses,javaSource);
+        inspection.runInspection();
 
-  //  Inspection inspection = new Inspection(loadedClasses,javaSource);
-
-    return 0;
-    }
-    
-    public double result() {
-    ArrayList<Class> loadedClasses = new ArrayList<>(directoryReader.getLoadedClasses()); // classes ready for reflection
-    ArrayList<File> javaSource = new ArrayList<>(directoryReader.getJavaSourceArrayList()); // can read java files as text
-    Inspection inspection = new Inspection(loadedClasses,javaSource);
-    inspection.runInspection();
-
-    final String FEATURE_ENVY = "FeatureEnvy";
-    final String LARGE_CLASS = "LargeClass";
-    final String LONG_METHOD = "LongMethod";
-    final String LONG_PARAM = "LongParameter";
-
-    Double featureEnvyPercent = inspection.getReports().get(FEATURE_ENVY).getPercentage();
-    Double largeClassPercent = inspection.getReports().get(LARGE_CLASS).getPercentage();
-    Double longMethodPercent = inspection.getReports().get(LONG_METHOD).getPercentage();
-    Double longParameterPercent = inspection.getReports().get(LONG_PARAM).getPercentage();
-
-    System.out.println("feature envy percent " + Math.round(featureEnvyPercent*100.0)/100.0);
-    System.out.println("large class percent " + Math.round(largeClassPercent*100.0)/100.0);
-    System.out.println("long method percent " + Math.round(longMethodPercent*100.0)/100.0);
-    System.out.println("long parameter percent " + Math.round(longParameterPercent * 100.0)/100.0);  
-    
-    return Math.round(longMethodPercent*100.0)/100.0;
+        final String FEATURE_ENVY = "FeatureEnvy";
+        final String LARGE_CLASS = "LargeClass";
+        final String LONG_METHOD = "LongMethod";
+        final String LONG_PARAM = "LongParameter";
+        	
+        double featureEnvyPercent = Math.round(inspection.getReports().get(FEATURE_ENVY).getPercentage());
+        double largeClassPercent = Math.round(inspection.getReports().get(LARGE_CLASS).getPercentage());
+        double longMethodPercent = Math.round(inspection.getReports().get(LONG_METHOD).getPercentage());
+        double longParameterPercent = Math.round(inspection.getReports().get(LONG_PARAM).getPercentage());
+        
+        // TODO: Change to real values when ready
+        double duplicateCodePercent = Math.round(inspection.getReports().get(FEATURE_ENVY).getPercentage());
+        double longclassPercent = Math.round(inspection.getReports().get(LARGE_CLASS).getPercentage());
+        double inappropriateIntimacyPercent = Math.round(inspection.getReports().get(LONG_METHOD).getPercentage());
+        double tooManyLiteralsPercent = Math.round(inspection.getReports().get(LONG_PARAM).getPercentage());   
+       
+        double primitiveObsessionsPercent = Math.round(inspection.getReports().get(FEATURE_ENVY).getPercentage());
+        double godComplexPercent = Math.round(inspection.getReports().get(LARGE_CLASS).getPercentage());
+        double lazyClassPercent = Math.round(inspection.getReports().get(LONG_METHOD).getPercentage());
+        double commentsPercent = Math.round(inspection.getReports().get(LONG_PARAM).getPercentage());
+        
+        double switchStatementsPercent = Math.round(inspection.getReports().get(FEATURE_ENVY).getPercentage());
+        double dataClumpPercent = Math.round(inspection.getReports().get(LARGE_CLASS).getPercentage());
+        double largeMethodPercent = Math.round(inspection.getReports().get(LONG_METHOD).getPercentage());
+        
+        percentages.add(featureEnvyPercent);
+        percentages.add(largeClassPercent);
+        percentages.add(longMethodPercent);
+        percentages.add(longParameterPercent); 
+        percentages.add(primitiveObsessionsPercent);
+        percentages.add(godComplexPercent);
+        percentages.add(lazyClassPercent);
+        percentages.add(commentsPercent);         
+        percentages.add(switchStatementsPercent);
+        percentages.add(dataClumpPercent);
+        percentages.add(largeMethodPercent);
+        
+        return percentages;     
     }
 }
 
