f8e9531ae2186f85a25270e9c4d046407c5b15b6
RichieBrady <richard.brady@ucdconnect.ie>
2019-04-22 19:37:38 +0100

    old name inspectable

--- src/Interface/Smellable.java
+++ src/Interface/Smellable.java
@@ -3,6 +3,6 @@ package Interface;
 import java.text.DecimalFormat;
 
 // interface for classes which reflect and read source
-public interface Inspectable extends SourceReadable, Reflectable{
+public interface Smellable extends SourceReadable, Reflectable{
     DecimalFormat df = new DecimalFormat("#.00");
 }
